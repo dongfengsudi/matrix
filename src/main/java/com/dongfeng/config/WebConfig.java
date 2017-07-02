4cb16962557468b1a2aa227482ea56b552766b11
muying.xx <muying.xx@alibaba-inc.com>
2017-07-02 17:05:20 +0800

    add css

---
+++ src/main/java/com/dongfeng/config/WebConfig.java
@@ -0,0 +1,25 @@
+package com.dongfeng.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.ResourceUtils;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * @author muying.xx
+ * @date 2017-07-02
+ */
+@EnableWebMvc
+@Configuration
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+        registry.addResourceHandler("/templates/**").addResourceLocations(ResourceUtils.CLASSPATH_URL_PREFIX+"/templates/");
+        registry.addResourceHandler("/static/**").addResourceLocations(ResourceUtils.CLASSPATH_URL_PREFIX+"/static/");
+        super.addResourceHandlers(registry);
+    }
+
+}

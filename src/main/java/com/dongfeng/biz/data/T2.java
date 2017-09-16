4d367c71f9b5f53a18c80597dd028423097cfbc0
zihan <472704698@qq.com>
2017-09-16 11:27:04 +0800

    add mysql config

---
+++ src/main/java/com/dongfeng/biz/data/T2.java
@@ -0,0 +1,18 @@
+package com.dongfeng.biz.data;
+
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * Created by zihan on 2017/9/16.
+ */
+
+ @Entity
+ @Table(name = "t2")
+public class T2 {
+    private int id;
+    private String name;
+
+
+}

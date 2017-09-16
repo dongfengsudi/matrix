700afc2bb045fa98501d7f4de61a7b3526c4e9f0
zihan <472704698@qq.com>
2017-09-16 20:06:19 +0800

    user path

--- src/main/java/com/dongfeng/admin/controller/AdminController.java
+++ src/main/java/com/dongfeng/admin/controller/AdminController.java
@@ -1,6 +1,15 @@
 package com.dongfeng.admin.controller;
 
+import com.dongfeng.biz.service.CommunityPageService;
+import com.dongfeng.biz.vo.CommunityCellVO;
+import com.dongfeng.biz.vo.CommunityPageVO;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * @author muying.xx
@@ -8,5 +17,12 @@ import org.springframework.stereotype.Controller;
  */
 @Controller
 public class AdminController {
+
+    @Resource
+    private CommunityPageService communityPageService;
+    @RequestMapping("login")
+    public String root(Model model) {
+        return "login";
+    }
 
 }

a20736af8b4f4b62069b086efdc1e30b6f7e4b06
muying.xx <muying.xx@alibaba-inc.com>
2017-09-16 14:33:49 +0800

    add banner opt

--- src/main/java/com/dongfeng/controller/test/TestController.java
+++ src/main/java/com/dongfeng/controller/test/TestController.java
@@ -1,12 +1,15 @@
 package com.dongfeng.controller.test;
 
-import com.dongfeng.biz.data.T1;
+import com.dongfeng.biz.data.BannerDO;
+import com.dongfeng.biz.repository.BannerRepository;
 import com.dongfeng.biz.repository.T1Repository;
+import io.vavr.control.Try;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.util.Date;
 
 /**
  * @author muying.xx
@@ -19,13 +22,26 @@ public class TestController {
     @Resource
     private T1Repository t1Repository;
 
-    @RequestMapping("/t1.htm")
-    public String testT1(@RequestParam(value="id") int id) {
-        T1 t1 = t1Repository.findById(id);
-        if (t1 != null) {
-            return t1.getName();
+    @Resource
+    private BannerRepository bannerRepository;
+
+    @RequestMapping("/banner/insert.htm")
+    public String insertData(@RequestParam(value="pic") String pic,
+                             @RequestParam(value="link") String link,
+                             @RequestParam(value="identity") String identity) {
+
+        BannerDO bannerDO = new BannerDO();
+        bannerDO.setDeleted((byte) 0);
+        bannerDO.setGmtCreate(new Date());
+        bannerDO.setGmtModified(new Date());
+        bannerDO.setLink(link);
+        bannerDO.setPicUrl(pic);
+        bannerDO.setTabIdentity(identity);
+
+        if (Try.of(() -> bannerRepository.save(bannerDO)).isSuccess()) {
+            return "success";
         } else {
-            return "find nothing";
+            return "fail";
         }
     }
 

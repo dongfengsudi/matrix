822b21aef789c733308d5580700def5d546caa21
muying.xx <muying.xx@alibaba-inc.com>
2017-09-16 14:36:58 +0800

    add package

---
+++ src/main/java/com/dongfeng/admin/controller/AdminController.java
@@ -0,0 +1,12 @@
+package com.dongfeng.admin.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author muying.xx
+ * @date 2017-09-16
+ */
+@Controller
+public class AdminController {
+
+}

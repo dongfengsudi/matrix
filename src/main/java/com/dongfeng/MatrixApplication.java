bb3c3a63afc84f9c0b6b7726cf80d3daff5638ef
muying.xx <muying.xx@alibaba-inc.com>
2017-09-20 20:13:35 +0800

    add encdoe

--- src/main/java/com/dongfeng/MatrixApplication.java
+++ src/main/java/com/dongfeng/MatrixApplication.java
@@ -1,9 +1,11 @@
 package com.dongfeng;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableEncryptableProperties
 public class MatrixApplication {
 
 	public static void main(String[] args) {
